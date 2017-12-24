08e543066fff64e10d6500be7dddb5645cb81df6
etnom <montgomery.okdaily@gmail.com>
2017-12-24 09:01:06 -0800

    import motor hat and python stuff for motor and blaster control

---
+++ turret.py
@@ -0,0 +1,8 @@
+# Made for Let's Robot
+# By Monty C
+
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_StepperMotor, Adafruit_DCMotor
+import time
+import atexit
+import RPi.GPIO as GPIO
+import threading
